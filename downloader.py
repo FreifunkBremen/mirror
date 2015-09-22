6499407a0b036df0c3dea59a99e9fb0e2144b2d9
ec8or <ec8or@posteo.de>
2015-09-22 19:55:37 +0200

    Download manifest only if never tahtn local. Check signatures of manifest before downloading images. Download only files/images mentioned in manifest

--- downloader.py
+++ downloader.py
@@ -1,58 +1,155 @@
 #!/usr/bin/python
 #
-# example call:
-#
-# ./downloader http://downloads.bremen.freifunk.net/firmware/
+# ./download.py http://www.example.com/ index.html
 #
 
+
 import sys
-import subprocess
+import os
+import datetime
+from dateutil import parser
+import urllib2
 
 
 class Downloader:
-    def __init__(self, base_url, output_dir=""):
-        self.base_url = base_url
-        self.output_dir = output_dir
+    def download(self, url, destination):
+        request = urllib2.Request(url)
 
-    def download(self):
-        command = ["wget"]
+        print(str.format("From {0}", url))
+        print(str.format("To {0}", destination))
 
-        # -P Set directory prefix to prefix. The directory
-        # prefix is the directory where all other files
-        # and sub-directories will be saved to, i.e.
-        # the top of the retrieval tree.
-        # The default is . (the current directory).
-        if self.output_dir != "":
-            command.extend(["-P", self.output_dir])
+        # Get modification time
+        if os.path.isfile(destination):
+            unix = int(os.path.getmtime(destination))
+            request.headers["if-modified-since"] = datetime.datetime.fromtimestamp(unix).strftime('%a, %d %b %Y %H:%M:%S GMT')
 
-        # -N don't re-retrieve files unless newer than local
-        command.extend(["-N"])
+        try:
+            opener = urllib2.build_opener()
+            stream = opener.open(request)
+        except urllib2.HTTPError, e:
+            if e.code == 304:
+                # Not modified
+                return False
+            raise
 
-        # recursively
-        command.extend(["-r"])
+        # Write to file
+        with open(destination, 'w') as f:
+            f.write(stream.read())
 
-        # No parents
-        command.extend(["-np"])
+        # Set modified
+        modified = parser.parse(stream.headers["last-modified"])
+        unix = int(modified.strftime("%s"))
+        os.utime(destination, (unix, unix))
 
-        # Load firmware and manifests only
-        command.extend(["-A", ".bin,.image,.manifest"])
+        # File modified
+        return True
 
-        # don't create the subdirectory downloads
-        command.extend(["-nH"])
+# if download(sys.argv[1], sys.argv[2]):
+#     print "modified"
+# else:
+#     print "not modified"
 
-        # don't create the sub directory firmware/
-        command.extend(["--cut-dirs=1"])
 
-        # ignore the remote directory nightly, its just a symlink
-        command.extend(["-X", "/firmware/nightly"])
-
-        # the url
-        command.extend([self.base_url])
-
-        # Start the subprocess
-        process = subprocess.Popen(command, stdin=subprocess.PIPE)
-        process.wait()
-
-l = Downloader(sys.argv[1])
-l.download()
-print("Download finished")
+# #!/usr/bin/python
+# #
+# # example call:
+# #
+# # ./downloader http://downloads.bremen.freifunk.net/firmware/
+# #
+# 
+# import sys
+# import subprocess
+# 
+# 
+# class Downloader:
+#         def __init__(self, base_url, output_dir=""):
+#                 self.base_url = base_url
+#                 self.output_dir = output_dir
+# 
+#         def download_manifests(self):
+#                 command = ["wget"]
+# 
+#                 # -P Set directory prefix to prefix. The directory
+#                 # prefix is the directory where all other files
+#                 # and sub-directories will be saved to, i.e.
+#                 # the top of the retrieval tree.
+#                 # The default is . (the current directory).
+#                 if self.output_dir != "":
+#                         command.extend(["-P", self.output_dir])
+# 
+#                 # -N don't re-retrieve files unless newer than local
+#                 command.extend(["-N"])
+# 
+#                 # recursively
+#                 command.extend(["-r"])
+# 
+#                 # No parents
+#                 command.extend(["-np"])
+# 
+#                 # Don't scream
+#                 command.extend(["--no-verbose"])
+# 
+#                 # Load firmware and manifests only
+#                 command.extend(["-A", ".manifest"])
+# 
+#                 # don't create the subdirectory downloads
+#                 command.extend(["-nH"])
+# 
+#                 # don't create the sub directory firmware/
+#                 command.extend(["--cut-dirs=1"])
+# 
+#                 # ignore the remote directory nightly, its just a symlink
+#                 command.extend(["-X", "/firmware/nightly"])
+# 
+#                 # the url
+#                 command.extend([self.base_url])
+# 
+#                 # Start the subprocess
+#                 process = subprocess.Popen(command, stdin=subprocess.PIPE)
+#                 process.wait()
+# 
+#         def download_all(self):
+#                 command = ["wget"]
+# 
+#                 # -P Set directory prefix to prefix. The directory
+#                 # prefix is the directory where all other files
+#                 # and sub-directories will be saved to, i.e.
+#                 # the top of the retrieval tree.
+#                 # The default is . (the current directory).
+#                 if self.output_dir != "":
+#                         command.extend(["-P", self.output_dir])
+# 
+#                 # -N don't re-retrieve files unless newer than local
+#                 command.extend(["-N"])
+# 
+#                 # recursively
+#                 command.extend(["-r"])
+# 
+#                 # No parents
+#                 command.extend(["-np"])
+# 
+#                 # Don't scream
+#                 command.extend(["--no-verbose"])
+# 
+#                 # Load firmware and manifests only
+#                 command.extend(["-A", ".bin,.image,.manifest"])
+# 
+#                 # don't create the subdirectory downloads
+#                 command.extend(["-nH"])
+# 
+#                 # don't create the sub directory firmware/
+#                 command.extend(["--cut-dirs=1"])
+# 
+#                 # ignore the remote directory nightly, its just a symlink
+#                 command.extend(["-X", "/firmware/nightly"])
+# 
+#                 # the url
+#                 command.extend([self.base_url])
+# 
+#                 # Start the subprocess
+#                 process = subprocess.Popen(command, stdin=subprocess.PIPE)
+#                 process.wait()
+# 
+# #l = Downloader(sys.argv[1])
+# #l.download()
+# #print("Download finished")
--- gluon_manifest.py
+++ gluon_manifest.py
@@ -5,18 +5,23 @@
 # ./gluon-manifest stable.manifest site.conf
 #
 
-import sys
 import subprocess
 from slpp import slpp as lua
 
+
 class Manifest:
+    NAME = 0
+    VERSION = 1
+    HASH_SUM = 2
+    FILE_NAME = 3
+
     def __init__(self, path):
-        self.path       = path
-        self.state      = 'start'
-        self.lines      = '' # lines to verify
-        self.entries    = []
+        self.path = path
+        self.state = 'start'
+        self.lines = ''  # lines to verify
+        self.firmwares = []
         self.signatures = []
-        self.vars       = dict()
+        self.vars = dict()
         self._parse()
 
     # returns the current branch
@@ -25,16 +30,16 @@ class Manifest:
 
     # return whether enough signatures are valid
     def verify(self, site_conf):
-        config  = self.siteConf(site_conf)['autoupdater']['branches'][self.branch()]
+        config = self.siteConf(site_conf)['autoupdater']['branches'][self.branch()]
         command = ["ecdsaverify", "-n", str(config['good_signatures'])]
 
         # Add pubkeys
         for key in config['pubkeys']:
-            command.extend(["-p",key])
+            command.extend(["-p", key])
 
         # Add signatures
         for sig in self.signatures:
-            command.extend(["-s",sig])
+            command.extend(["-s", sig])
 
         # Start the subprocess
         proc = subprocess.Popen(command, stdin=subprocess.PIPE)
@@ -49,39 +54,38 @@ class Manifest:
 
     # the parsed site.conf
     def siteConf(self, site_conf):
-        with open(site_conf,'r') as f:
+        with open(site_conf, 'r') as f:
             return lua.decode(f.read())
 
     def _parse(self):
-        with open(self.path,'r') as f:
+        with open(self.path, 'r') as f:
             for line in f:
                 # parse the line
-                getattr(self, "_"+self.state)(line.strip())
+                getattr(self, "_" + self.state)(line.strip())
 
                 # data do verify
                 if self.state != "signatures":
                     self.lines += line
 
-    def _start(self,line):
+    def _start(self, line):
         if line == "":
             self.state = "files"
         else:
-            k,v = line.split("=",2)
+            k, v = line.split("=", 2)
             self.vars[k] = v
 
-    def _files(self,line):
+    def _files(self, line):
         if line == "---":
             self.state = "signatures"
         else:
-            self.entries.append(line.split(" "))
+            self.firmwares.append(line.split(" "))
 
-    def _signatures(self,line):
+    def _signatures(self, line):
         self.signatures.append(line)
 
+# mf = Manifest(sys.argv[1])
 
-mf = Manifest(sys.argv[1])
-
-if mf.verify(sys.argv[2]):
-    print("signatures valid")
-else:
-    print("signatures invalid")
+# if mf.verify(sys.argv[2]):
+#    print("signatures valid")
+# else:
+#    print("signatures invalid")
